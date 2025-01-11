6ac74df78b6088ca58e7f55118be1d94bdc52ec9
tasukuwatanabe <watanabe.tasuku@hrbrain.co.jp>
2025-01-12 00:02:28 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./components/home/Home";
+import Sidebar from "./components/sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import "./Sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-left">Left</div>
+      <div className="sidebar-right">Right</div>
+    </div>
+  );
+};
+
+export default Sidebar;

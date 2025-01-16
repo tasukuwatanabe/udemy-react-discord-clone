71bf4acebf24d4129e15f8f505bacca821c86a85
tasukuwatanabe <watanabe.tasuku@hrbrain.co.jp>
2025-01-16 23:59:13 +0900

    Firebaseを使ってGoogleログイン機能を実装してみよう

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,14 +1,22 @@
+import { signInWithPopup } from "firebase/auth";
 import { Button } from "@mui/material";
 import "./Login.scss";
+import { auth, provider } from "../../firebase";
 
 const Login = () => {
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch((err) => {
+      alert(err.message);
+    });
+  };
+
   return (
     <div className="login">
       <div className="loginLogo">
         <img src="/logo_discord.png" alt="" />
       </div>
 
-      <Button>ログイン</Button>
+      <Button onClick={signIn}>ログイン</Button>
     </div>
   );
 };

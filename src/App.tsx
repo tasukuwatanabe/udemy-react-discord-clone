ab7a98130ecb0e9625574ec90e1874a02976e30b
tasukuwatanabe <watanabe.tasuku@hrbrain.co.jp>
2025-01-17 12:13:23 +0900

    ログインしたユーザー情報をStoreに通知して状態更新しよう

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { useEffect } from "react";
 import "./App.scss";
-import { useAppSelector } from "./app/hooks";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
 import Chat from "./components/chat/Chat";
 import Login from "./components/login/Login";
 import Sidebar from "./components/sidebar/Sidebar";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 
 function App() {
   const user = useAppSelector((state) => state.user);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser);
+      if (loginUser) {
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            photo: loginUser.photoURL,
+            email: loginUser.email,
+            displayName: loginUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
 
   return (
     <div className="App">
